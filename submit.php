6662ebe1d84a3f41ba85cc7214140ffe53987aa2
triagustinaputri <42725273+triagustinaputri@users.noreply.github.com>
2018-10-05 12:10:05 +0700

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,20 +1,22 @@
-<?php
-require_once("db.php");
+<?php 
+require_once("connect.php");
 
-$nama       = $_POST['nama'];
-$nim        = $_POST['nim'];
-$tgl_lahir  = $_POST['tgl_lahir'];
+$nama = $_POST['nama'];
+$nim = $_POST['nim'];
+$tgl_lahir = $_POST['tgl_lahir'];
 
-$sql = "INSERT INTO siswa(nama, nim, tgl_lahir) 
-        VALUES ('$nama','$nim','$tgl_lahir')";
+$sql = "INSERT INTO siswa(nim, nama, tgl_lahir)
+		VALUES ('$nim', '$nama', '$tgl_lahir')";
+
 
 
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully<br>";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
+	echo "New record created successfully";
+
+}else{
+	echo "Error: ". $sql . "<br>" . mysqli_error($conn);
+}		
 
 mysqli_close($conn);
-echo "silahkan klik <a href='list.php'>link</a> untuk ke halaman list";
+echo "Silahkan klik <a href='list.php'>link ini</a> untuk selanjutnya";
 ?> 
